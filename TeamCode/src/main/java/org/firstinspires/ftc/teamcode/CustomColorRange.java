16749c87fb8ba2e497f35c2f3344b7e46cd0a234
WSRWaveDroids <wavedroid13206@gmail.com>
2025-09-25 20:07:04 -0500

    Copied over a lot of stable files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomColorRange.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.INTO_THE_DEEP_BOT;
+
+import org.firstinspires.ftc.vision.opencv.ColorRange;
+import org.firstinspires.ftc.vision.opencv.ColorSpace;
+import org.opencv.core.Scalar;
+
+public class CustomColorRange extends ColorRange {
+    protected final ColorSpace colorSpace;
+    protected final Scalar min;
+    protected final Scalar max;
+
+    // -----------------------------------------------------------------------------
+    // DEFAULT OPTIONS
+    // -----------------------------------------------------------------------------
+
+    public static final ColorRange ARTIFACT_PURPLE = new ColorRange(
+            ColorSpace.HSV,
+            new Scalar(134,  70,  70),
+            new Scalar(154, 255, 255)
+    );
+
+    public static final ColorRange ARTIFACT_GREEN = new ColorRange(
+            ColorSpace.HSV,
+            new Scalar(55,  50,  70),
+            new Scalar(85, 255, 255)
+    );
+
+    public static final ColorRange EVERYTHING = new ColorRange(
+            ColorSpace.RGB,
+            new Scalar (0,0,0),
+            new Scalar (255,255,255)
+    );
+
+    public CustomColorRange(ColorSpace colorSpace, Scalar min, Scalar max) {
+        super(colorSpace,min,max);
+        this.colorSpace = colorSpace;
+        this.min = min;
+        this.max = max;
+    }
+}
