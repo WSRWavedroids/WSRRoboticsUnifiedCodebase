5ecf57315e529b2e5b8c6ba39eedf21735984aee
wsrsh <dcarter26@wsr.k12.ia.us>
2025-11-08 15:21:37 -0600

    pushing stuff to the pushed code lines with pushed code from pushes pushed previously to the pushed location

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Basic_TeleOp_NewBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Basic_TeleOp_NewBot.java
@@ -127,6 +127,7 @@ public class Basic_TeleOp_NewBot extends OpMode {
 
         intake1(1 ,0.5 ,0.85);
         intake2(0.5 ,0.2 ,1);
+
         launch(0.1 ,0.62 ,0.73 );
 
         //Matthew Was Here
