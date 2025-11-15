ba55b2987e6e6da53c5148f1cb8e4332131a4821
wsrsh <dcarter26@wsr.k12.ia.us>
2025-11-15 12:01:36 -0600

    This is new auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueOnLaunchZoneANGLED.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Robot;
+
+@Autonomous(group =  "Auto", name = "RedOnLaunchZoneANGLED")
+public class RedOnLaunchZoneANGLED extends AutonomousPLUS {
+
+    private Robot robot;
+
+    @Override
+    public void runOpMode() {
+        super.runOpMode();
+        robot = new Robot(hardwareMap, telemetry, this);
+
+        waitForStart();
+        //Under This Is Were You Put Stuff
+        //900 tick = about 20 in
+        //550 ticks = about 90 degrees right
+        //6 second pause after everything
+        moveRobotBackward(1400, 6, 0.4);
+        runLauncherAuto(0.32);
+        sleep(1500);
+        runIntake2Auto(0.5);
+        runIntakeAuto("In");
+        sleep(2000);
+        prepareNextAction(4);
+        runLauncherAuto(0);
+        runIntake2Auto(0);
+        runIntakeAuto("Stop");
+        prepareNextAction(6);
+        moveRobotRight(1200,6,0.2); 
+        //turnRobotLeft(1650, 6);
+        //moveRobotLeft(900,6,0.4);
+        //runIntakeAuto(In);          //this is intake
+        //moveRobotForward(750,6,40);
+        //sleep(2000);
+        //runIntakeAuto(Stop);
+        //moveRobotBackward(750,6,40);         //this is launch
+        //moveRobotLeft(450,6,40);
+        //turnRobotRight(1650,6,40);
+        //runLauncherAuto(0.7);
+        //sleep(1500);
+        //runIntake2Auto(1);
+        //runIntakeAuto(In);
+        //sleep(4000);
+        //runLauncherAuto(0);
+        //runIntake2Auto(0);
+        //runIntakeAuto(Stop);
+    }
+}
+//whatever i want
+//Jarett Kaedan Butler was here
