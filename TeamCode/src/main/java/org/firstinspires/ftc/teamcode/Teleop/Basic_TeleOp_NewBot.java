82298a6e0651ba7c3b92dbda7cbe190666b119e7
wsrsh <dcarter26@wsr.k12.ia.us>
2025-11-18 20:14:01 -0600

    telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Basic_TeleOp_NewBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Basic_TeleOp_NewBot.java
@@ -318,8 +318,8 @@ public class Basic_TeleOp_NewBot extends OpMode {
         robot.launchRight.setPower(1);
         robot.launchSpeed = input - robot.triggerDeadzone;
         robot.launchSpeed = robot.launchSpeed + robot.triggerDeadzone * input;
-        robot.launchLeft.setVelocity(-robot.launchSpeed * robot.launchTune * 6000);
-        robot.launchRight.setVelocity(robot.launchSpeed * robot.launchTune * 6000);
+        robot.launchLeft.setVelocity(-robot.launchSpeed * robot.launchTune);
+        robot.launchRight.setVelocity(robot.launchSpeed * robot.launchTune);
         if (gamepad2.right_stick_button || gamepad2.left_stick_button) {
             telemetry.addLine("noahguywashere");
             telemetry.addLine("DevenWasHere");
@@ -368,10 +368,10 @@ public class Basic_TeleOp_NewBot extends OpMode {
 
         robot.triggerDeadzone = deadzone;
         if (gamepad2.right_trigger > robot.triggerDeadzone) {
-            robot.launchTune = pwrNormal;
+            robot.launchTune = pwrNormal * 6000;
             setLaunchPower(gamepad2.right_trigger);
         } else if (gamepad2.left_trigger > robot.triggerDeadzone) {
-            robot.launchTune = pwrHigh;
+            robot.launchTune = pwrHigh * 6000;
             setLaunchPower(gamepad2.left_trigger);
         } else {
             robot.launchLeft.setVelocity(0);
