a159bfd84280ee8548bbce642dd8e08707be0c83
Michael Walbeck <wavedroid13206@gmail.com>
2026-02-14 19:43:18 -0600

    Removed the blinkify() function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/SlotLightManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/SlotLightManager.java
@@ -8,7 +8,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class SlotLightManager {
 
     private final Robot robot;
-    private final ElapsedTime blinkyTimer;
     private final ElapsedTime timeSinceUpdate;
     public Light fireLight;
     public Light loadLight;
@@ -20,7 +19,6 @@ public class SlotLightManager {
         loadLight = new Light(LOAD, robot.loadRGB);
         storeLight = new Light(STORE, robot.storeRGB);
 
-        blinkyTimer = new ElapsedTime();
         timeSinceUpdate = new ElapsedTime();
     }
 
@@ -36,25 +34,15 @@ public class SlotLightManager {
     public class Light {
         SorterHardware.PositionState position;
         Servo light;
-        boolean blinky;
         public Light(SorterHardware.PositionState position, Servo light) {
             this.position = position;
             this.light = light;
         }
 
         public void update() {
-            final int hertz = 2;
-            if (blinky && ((int) blinkyTimer.seconds()) * hertz % (hertz * 2) == 0) {
-                light.setPosition(0);
-            } else {
-                light.setPosition(robot.sorterLogic.findCurrentSlotInPosition(position)
-                        .getOccupied()
-                        .lightColor);
-            }
-        }
-
-        public void blinkify(boolean blink) {
-            blinky = blink;
+            light.setPosition(robot.sorterLogic.findCurrentSlotInPosition(position)
+                    .getOccupied()
+                    .lightColor);
         }
     }
 }
