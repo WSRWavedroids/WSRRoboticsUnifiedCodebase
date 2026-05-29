f153f0f0da54d078148e1f383dedcbb0de78b9cf
wsrsh <dcarter26@wsr.k12.ia.us>
2026-05-28 21:01:12 -0500

    PedroBot Stuff:)

--- TeamCode/src/main/java/org/firstinspires/ftc/pedroBot/PathPlan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/pedroBot/PathPlan.java
@@ -1,5 +1,16 @@
 package org.firstinspires.ftc.pedroBot;
 
+import com.pedropathing.geometry.Pose;
+
 public class PathPlan {
     double score;
+    public Pose goal;
+    public Pose robot;
+    double robotangle;
+    double offset;
+    double length;
+    double w;
+    double b;
+    double magnitudeToOffsetVectored;
+    double distanceToOffset;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/pedroBot/PedroBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/pedroBot/PedroBot.java
@@ -1,17 +1,32 @@
 package org.firstinspires.ftc.pedroBot;
 
-public class PedroBot {
-    PathPlan placeholder;
-    private PathPlan calculatePath(PathPlan input) {
-        // draw line directly to the goal
-        if (true) { // We didn't hit anything!
+public class PedroBot{
+
+    PathPlan pathPlan;
+    public PathPlan calculatePath(PathPlan input) {
+        // calculate straight line to goal
+        pathPlan.magnitudeToOffsetVectored = pathPlan.length - Math.abs(pathPlan.robotangle);
+        pathPlan.distanceToOffset = Math.sqrt(Math.pow(pathPlan.magnitudeToOffsetVectored *Math.cos(pathPlan.robotangle)-pathPlan.goal.getX(), 2) + Math.pow(pathPlan.magnitudeToOffsetVectored *Math.sin(pathPlan.robotangle)-pathPlan.goal.getY(), 2));
+
+        boolean hit;
+        if (pathPlan.distanceToOffset <= pathPlan.offset) {
+            hit = true;
+        }
+        else {
+            hit = false;
+        }
+
+        if (!hit) { // We didn't hit anything!
             return input;
         }
         else {
             // calculate one path
-            PathPlan optionOne = calculatePath(placeholder);
+            pathPlan.length = Math.sqrt((Math.pow(pathPlan.robot.getX() - pathPlan.goal.getX(), 2) + (Math.pow(pathPlan.robot.getY() - pathPlan.goal.getY(), 2))));
+            pathPlan.w = Math.sqrt(Math.pow(pathPlan.length, 2) - Math.pow(pathPlan.offset, 2));
+            pathPlan.b = pathPlan.robotangle + Math.asin((pathPlan.offset/ pathPlan.length));
+            PathPlan optionOne = calculatePath(pathPlan);
             // calulate the other path
-            PathPlan optionTwo = calculatePath(placeholder);
+            PathPlan optionTwo = calculatePath(pathPlan);
 
             if (optionOne.score >= optionTwo.score) {
                 return optionOne;
