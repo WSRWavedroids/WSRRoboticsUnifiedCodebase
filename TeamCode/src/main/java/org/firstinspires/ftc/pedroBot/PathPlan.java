3c80151b3c392f3648cface946b394b765158952
mwalbeck09 <mwalbeck09@gmail.com>
2026-06-02 14:21:08 -0500

    Add some feedback comments to PedroBot.java and PathPlan.java

--- TeamCode/src/main/java/org/firstinspires/ftc/pedroBot/PathPlan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/pedroBot/PathPlan.java
@@ -6,11 +6,22 @@ public class PathPlan {
     double score;
     public Pose goal;
     public Pose robot;
-    double robotangle;
-    double offset;
+    double robotangle; //TODO capitalization conventions -- Michael
+    double offset; //TODO please be more specific -- Michael
     double length;
     double w;
     double b;
-    double magnitudeToOffsetVectored;
+    double magnitudeToOffsetVectored; // TODO vectored? -- Michael
     double distanceToOffset;
+
+    /**
+     * Here's a fun feature. This is called a Javadoc. It's a documentation method that can give a
+     * description to a variable, class, or function. Hover over the word "example" below to see it.
+     * <p>
+     * You can add these to the more confusing bits of your code to explain how something is
+     * intended to be used or to provide more details than a name would allow.
+     * <p>
+     * -- Michael
+     */
+    Object example;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/pedroBot/PedroBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/pedroBot/PedroBot.java
@@ -1,13 +1,40 @@
 package org.firstinspires.ftc.pedroBot;
 
-public class PedroBot{
+public class PedroBot {
 
+    /*
+    Alright...
+
+    This looks pretty good right now except for one major issue. Your pathPlan object exists
+    globally, outside the function. That means that every branch of your recursion tree (you get the
+    metaphor) is editing the same PathPlan. You could give the PathPlan class a tree-like structure
+    itself, but that seems like a lot of work compared to what you could do. I set up the
+    calculatePath() function with a pathPlan as an input and output. Rather than having each node in
+    the tree edit a global object, give each node its own PathPlan object (input) and have it modify
+    that. That way, the tree-like structure is built in. In other words, pass the data through the
+    function inputs, not by storing it outside of the tree.
+
+    I've left comments all around this file and PathPlan.java. You're doing really well, just a
+    handful of readability issues here and there that make the logic difficult to parse.
+
+    -- Michael
+     */
     PathPlan pathPlan;
     public PathPlan calculatePath(PathPlan input) {
         // calculate straight line to goal
+        /*
+        What is this math doing? What's offset? What does "vectored" mean?
+
+        -- Michael
+         */
         pathPlan.magnitudeToOffsetVectored = pathPlan.length - Math.abs(pathPlan.robotangle);
         pathPlan.distanceToOffset = Math.sqrt(Math.pow(pathPlan.magnitudeToOffsetVectored *Math.cos(pathPlan.robotangle)-pathPlan.goal.getX(), 2) + Math.pow(pathPlan.magnitudeToOffsetVectored *Math.sin(pathPlan.robotangle)-pathPlan.goal.getY(), 2));
 
+        /*
+        These two if-else blocks can be simplified into one
+
+        -- Michael
+         */
         boolean hit;
         if (pathPlan.distanceToOffset <= pathPlan.offset) {
             hit = true;
