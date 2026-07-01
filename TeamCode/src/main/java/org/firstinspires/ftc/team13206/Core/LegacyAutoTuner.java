5b375e7f0b8bacf99990831af7d5dbb048f0f860
mwalbeck09 <mwalbeck09@gmail.com>
2026-07-01 11:00:36 -0500

    Rework LegacyAutoTuner as a static utility instead of an opMode.

--- TeamCode/src/main/java/org/firstinspires/ftc/team13206/Core/LegacyAutoTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team13206/Core/LegacyAutoTuner.java
@@ -1,181 +1,66 @@
 package org.firstinspires.ftc.team13206.Core;
 
-import static org.firstinspires.ftc.team13206.Core.LegacyAutoTuner.mode.*;
-
 import com.bylazar.configurables.annotations.Configurable;
-import com.bylazar.panels.Panels;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
-@Disabled
+/**
+ * This utility is intended to make creating a standard legacy teleop easier. Call the
+ * findNextDistance() function at a point in the auto to stop the program and construct the next
+ * movement using Panels. To access Panels, go to 192.168.43.1:8001 on the robot Wi-Fi.
+ * <p>
+ * The values you can change in Panels are:
+ * </p>
+ * - direction: The direction to move.
+ * <p>
+ * - distance: The distance to move, measured in motor ticks.
+ * </p>
+ * - done: When set to true, will cause the function to return. It will automatically set
+ * itself back to false.
+ * </p>
+ *
+ */
 @Configurable
-@Autonomous(group = "Basic", name = "Legacy Auto Tuner")
-public class LegacyAutoTuner extends AutonomousPLUS {
+public class LegacyAutoTuner {
 
-    /// ///192.168.43.1:8001  IP Address of panels, type this into your browser when on robot wifi
-    /// Opening panels will allow you to change the values effecting the built in motor PIDS
-    /// REMEMBER! Changing variables in panels DOES NOT change the hard coded value after runtime
-    /// In order to save your changes, type them into the script, replacing the values here
-    ///
-    /// To change movement types, press the bumpers to cycle through, and read the driver hub screen
-    /// To pause and unpause, press square
-    ///
-    /// If your movements don't work as intended, please reference your autonomousPLUS file
-    /// and change the movements in this script to match your autos
-    ///
-    /// Yes, I know this would be better as a state machine, but this way works with more robots / codebases
-    /// This script was written by Clay Kramer, FTC 13206 🌊🤖, 2025
+    /**
+     * The direction to move in.
+     */
+    private static Robot.MoveDirection direction;
 
-    private org.firstinspires.ftc.team13206.Core.Robot robot;
-    //modes
-    public enum mode {FB, LR, TRN, DGNL, TRI, PAUSE};
-    private mode savedMode;
-    //Panels changeable values
-    public static double p = 5;
-    public static int tolerance;
-    public static int movementDistance = 900;
-    public static double modularSpeed = 0.5;
-    public static long timeBetweenMovements = 200;
+    /**
+     * The distance, in ticks, to move.
+     */
+    public static int distance = 0;
 
-    public void runOpMode() {
-        super.runOpMode();
-        robot = new Robot(hardwareMap, telemetry, this);
-        robot.panels = Panels.INSTANCE;
-        mode current = mode.PAUSE;
-        waitForStart();
-        robot.resetDriveEncoders();
-        while(opModeIsActive())
-        {
-            if(current == mode.FB)
-            {
-                calibrateDriveTrain(tolerance, p);
-                speed = modularSpeed;
-                moveRobotForward(movementDistance, timeBetweenMovements);
-                calibrateDriveTrain(tolerance, p);
-                speed = modularSpeed;
-                moveRobotBackward(movementDistance, timeBetweenMovements);
-                changeModes(current);
-                if(gamepad1.square)
-                {
-                    current = PAUSE;
-                }
-            }
-            else if(current == mode.LR)
-            {
-                calibrateDriveTrain(tolerance, p);
-                speed = modularSpeed;
-                moveRobotLeft(movementDistance, timeBetweenMovements);
-                calibrateDriveTrain(tolerance, p);
-                speed = modularSpeed;
-                moveRobotRight(movementDistance, timeBetweenMovements);
-                changeModes(current);
-                if(gamepad1.square)
-                {
-                    current = PAUSE;
-                }
-            }
-            else if(current == mode.TRN)
-            {
-                calibrateDriveTrain(tolerance, p);
-                speed = modularSpeed;
-                turnRobotLeft(movementDistance, timeBetweenMovements);
-                calibrateDriveTrain(tolerance, p);
-                speed = modularSpeed;
-                turnRobotRight(movementDistance, timeBetweenMovements);
-                changeModes(current);
-                if(gamepad1.square)
-                {
-                    current = PAUSE;
-                }
-            }
-            else if(current == mode.DGNL)
-            {
-                calibrateDriveTrain(tolerance, p);
-                speed = modularSpeed;
-                moveDiagonalLeft(movementDistance, timeBetweenMovements);
-                calibrateDriveTrain(tolerance, p);
-                speed = modularSpeed;
-                moveDiagonalRight(movementDistance, timeBetweenMovements);
-                changeModes(current);
-                if(gamepad1.square)
-                {
-                    current = PAUSE;
-                }
-            }
-            else if(current == mode.TRI)
-            {
-                calibrateDriveTrain(tolerance, p);
-                speed = modularSpeed;
-                moveDiagonalRight((int) Math.sqrt((2* (movementDistance^2))), timeBetweenMovements); // Do a little trig lol
-                calibrateDriveTrain(tolerance, p);
-                speed = modularSpeed;
-                moveRobotBackward(movementDistance, timeBetweenMovements);
-                calibrateDriveTrain(tolerance, p);
-                speed = modularSpeed;
-                moveRobotLeft(movementDistance, timeBetweenMovements);
-                changeModes(current);
-                if(gamepad1.square)
-                {
-                    current = PAUSE;
-                }
-            }
-            if(current == mode.PAUSE)
-            {
-                telemetry.addData("Paused", savedMode);
-                calibrateDriveTrain(tolerance, p);
-                speed = modularSpeed;
-                changeModes(current);
-                if(gamepad1.square)
-                {
-                    current = mode.PAUSE;
-                }
-            }
-            telemetry.addData("Current Mode is:", savedMode);
-            telemetry.update();
-        }
-    }
+    /**
+     * Breaks the loop and returns the function. Will automatically set itself back to false.
+     */
+    public static boolean done = false;
 
-    void changeModes(mode current)
-    {
-        if (savedMode == mode.FB) {
-            if (gamepad1.dpad_right) {
-                current = mode.LR;
-                savedMode = current;
-            } else if (gamepad1.dpad_left) {
-                current = mode.TRI;
-                savedMode = current;
+    /**
+     * Call this function at a point in the auto to stop the program and construct the next
+     * movement using Panels. To access Panels, go to 192.168.43.1:8001 on the robot Wi-Fi.
+     * <p>
+     * The values you can change in Panels are:
+     * </p>
+     * - direction: The direction to move.
+     * <p>
+     * - distance: The distance to move, measured in motor ticks.
+     * </p>
+     * - done: When set to true, will cause the loop to end once the current movement is done. It will automatically set
+     * itself back to false.
+     * </p>
+     * Changing these values will not hardcode them in
+     * @param auto The AutonomousPLUS object the auto is using. Used to import things like the
+     *             hardware map and the move() function.
+     */
+    public static void findNextDistance(AutonomousPLUS auto) {
+        while (true) {
+            if (direction != null) {
+                auto.tentativeMove(direction, distance);
             }
-        } else if (savedMode == mode.LR) {
-            if (gamepad1.dpad_right) {
-                current = mode.TRN;
-                savedMode = current;
-            } else if (gamepad1.dpad_left) {
-                current = mode.FB;
-                savedMode = current;
-            }
-        } else if (savedMode == mode.TRN) {
-            if (gamepad1.dpad_right) {
-                current = mode.DGNL;
-                savedMode = current;
-            } else if (gamepad1.dpad_left) {
-                current = mode.LR;
-                savedMode = current;
-            }
-        } else if (savedMode == mode.DGNL) {
-            if (gamepad1.dpad_right) {
-                current = mode.TRI;
-                savedMode = current;
-            } else if (gamepad1.dpad_left) {
-                current = mode.TRN;
-                savedMode = current;
-            }
-        } else if (savedMode == mode.TRI) {
-            if (gamepad1.dpad_right) {
-                current = mode.FB;
-                savedMode = current;
-            } else if (gamepad1.dpad_left) {
-                current = mode.DGNL;
-                savedMode = current;
+            if (done && auto.checkMovement()) {
+                done = false;
+                return;
             }
         }
     }
