0b955a4810bdce2c251961e780c76bad4c70a06a
mwalbeck09 <mwalbeck09@gmail.com>
2026-07-13 11:51:34 -0400

    Trim TemplateLegacyLinearAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Templates/TemplateLegacyLinearAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Templates/TemplateLegacyLinearAuto.java
@@ -4,22 +4,19 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.Core.AutonomousPLUS;
 import org.firstinspires.ftc.teamcode.Core.Robot;
 
 @Disabled // TODO remove this if you ever want to run the program
 @Autonomous(group = "Templates", name = "Legacy Linear Autonomous")
 public class TemplateLegacyLinearAuto extends LinearOpMode {
     private Robot robot;
-    private AutonomousPLUS auto;
-
-    public static final String ALLIANCE_KEY = "Alliance";
-    public static final String PATTERN_KEY = "Pattern";
 
     public void runOpMode() {
 
         robot = new Robot(hardwareMap, telemetry, this);
-        auto = new AutonomousPLUS(robot);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
         while (opModeInInit()) {
             // This is the equivalent of init_loop(). It will repeat until the play button is
@@ -29,7 +26,6 @@ public class TemplateLegacyLinearAuto extends LinearOpMode {
 
 
         robot.readyHardware();
-        blackboard.put(ALLIANCE_KEY, "RED");
 
         waitForStart();
 
