b048a803c8cc9c9279d8323fa855d8095b3c9b18
mwalbeck09 <mwalbeck09@gmail.com>
2026-08-11 17:56:42 -0500

    Intentionally create a merge conflict

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Templates/TemplateLegacyLinearAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Templates/TemplateLegacyLinearAuto.java
@@ -31,7 +31,7 @@ public class TemplateLegacyLinearAuto extends LinearOpMode {
 
         //The code to run once play has been pressed goes here.
 
-
+        // Actually, I want it to look like this
     }
 }
 
