ea740e90a9a4c0575d658e07dad6605dd5a44816
wsr <wavedroid13206@gmail.com>
2026-08-21 21:02:31 -0500

    Session 9: Basket, Bagel Boy, Laundry, and 1/2 Bread Boy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/eight/Pink_Banana_Op.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/eight/Pink_Banana_Op.java
@@ -2,6 +2,10 @@ package org.firstinspires.ftc.teamcode.lessons.eight;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+import static org.firstinspires.ftc.teamcode.Core.LED.LEDColors.GREEN;
+import static org.firstinspires.ftc.teamcode.Core.LED.LEDColors.OFF;
+import static org.firstinspires.ftc.teamcode.Core.LED.LEDColors.RED;
+import static org.firstinspires.ftc.teamcode.Core.Robot.Color.PINK;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -33,10 +37,12 @@ public class Pink_Banana_Op extends OpMode {
 
     private double motorStartTime = 0;
 
+    private double ledStartTime = 0;
+
     private boolean motorIsRunning = false;
 
     private enum Step{
-        NOT_RUNNING, CLOCKWISE, COUNTERCLOCKWISE, FAST_COUNTERCLOCKWISE
+        NOT_RUNNING, CLOCKWISE, COUNTERCLOCKWISE, FAST_COUNTERCLOCKWISE, LED_RED, LED_GREEN, LED_OFF, MOVE_COLOR_SERVO, SENSING, AXON_BACK_FORTH, AXON_CENTER,
     }
     private Step step = Step.NOT_RUNNING;
     /*
@@ -75,29 +81,88 @@ public class Pink_Banana_Op extends OpMode {
     public void loop() {
         switch (step) {
             case NOT_RUNNING:
-                if (gamepad1.crossWasPressed()) {
-                    robot.motor.setPower(0.5);
+                if (gamepad1.cross) {
+                    robot.motor.setPower(-0.5);
                     motorStartTime = runtime.seconds();
                     step = Step.COUNTERCLOCKWISE;
                 }
                 break;
             case COUNTERCLOCKWISE:
                 if (runtime.seconds() - motorStartTime >= 2){
-                    robot.motor.setPower(-0.5);
+                    robot.motor.setPower(0.5);
                     motorStartTime = runtime.seconds();
                     step = Step.CLOCKWISE;
                 }
                 break;
             case CLOCKWISE:
-            if (runtime.seconds() - motorStartTime >= 2){
-                robot.motor.setPower(0.5);
-                motorStartTime = runtime.seconds()
+                if (runtime.seconds() - motorStartTime >= 2){
+                    robot.motor.setPower(-1.0);
+                    motorStartTime = runtime.seconds();
+                    step = Step.FAST_COUNTERCLOCKWISE;
+                }
+                break;
+            case FAST_COUNTERCLOCKWISE:
+                if (runtime.seconds() - motorStartTime >= 1) {
+                    robot.motor.setPower(0);
+                    motorStartTime = runtime.seconds();
+                    step = Step.NOT_RUNNING;
+                }
+
+                break;
+
+        }
+
+        telemetry.addData("Motor step", step);
+        telemetry.update();
+
+    switch (step) {
+        case NOT_RUNNING:
+            if (gamepad1.circle) {
+                robot.led.setColor(RED);
+                ledStartTime = runtime.seconds();
+                step = Step.LED_RED;
             }
             break;
-            case Step.FAST_COUNTERCLOCKWISE:
-                // Step 4 code
-                break;
+        case LED_RED:
+            if (runtime.seconds() - ledStartTime >= 1) {
+                robot.led.setColor(GREEN);
+                ledStartTime = runtime.seconds();
+                step = Step.LED_GREEN;
+            }
+            break;
+        case LED_GREEN:
+            if (runtime.seconds() - ledStartTime >= 2) {
+                robot.led.setColor(OFF);
+                ledStartTime = runtime.seconds();
+                step = Step.NOT_RUNNING;
+            }
+            break;
+
         }
+
+
+        telemetry.addData("LED step", step);
+        telemetry.update();
+
+        switch (step) {
+            case NOT_RUNNING:
+                if (gamepad1.square){
+                    robot.colorServo.setPower(0.1);
+                    step = Step.MOVE_COLOR_SERVO;
+                }
+                break;
+            case SENSING:
+                if (robot.getColor() == PINK);
+                robot.axonServo.setPosition(1);
+                robot.axonServo.setPosition(0);
+                step = Step.AXON_CENTER;
+                break;
+            case AXON_CENTER:
+                if (robot.button.isPressed()) {
+                    robot.axonServo.setPosition(.5);
+                    step = Step.NOT_RUNNING;
+                }
+                }
     }
 
     /**
