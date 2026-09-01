eb37d9d9c03dfd20dd47e7057153bac27f004f6c
WSRWaveDroids <wavedroid13206@gmail.com>
2026-08-31 21:03:17 -0500

    Create launcher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/Launcher.java
@@ -2,16 +2,19 @@ package org.firstinspires.ftc.teamcode.Core;
 
 public class Launcher {
     private Robot robot;
+
     public Launcher(Robot robot) {
         this.robot = robot;
     }
+
     private int launcherSpeed = 0;
     public boolean isLaunching = false;
     public boolean launched = false;
     public boolean readyToLaunch = false;
     private boolean flipperIsUp = true;
+
     //flipperIsUp is set to true initially until it is checked for extra safety
-    private enum launcherState{
+    private enum launcherState {
         STALL,
         CHECK_BLENDER,
         REV_MOTOR,
@@ -19,19 +22,23 @@ public class Launcher {
         FLICKY_DOWN
 
     }
+
     launcherState currentState = launcherState.STALL;
+
     public void fireBall() {
 
     }
-    public void update() {
 
+    public void update() {
 
 
         switch (currentState) {
             case STALL:
+                /*if (gamePadButton) {
+
+        }*/
 
         }
 
     }
-
 }
