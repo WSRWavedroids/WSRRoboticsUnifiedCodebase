a941dca63faa28d026f02c282f9d5ce80c81a481
Deven <dpahuja29@wsr.k12.ia.us>
2026-09-03 21:05:03 -0500

    LaunchSequence setup now I just need to put in functions - Deven

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/LaunchSequence.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/LaunchSequence.java
@@ -15,8 +15,35 @@ public class LaunchSequence {
     public void clearQueue() {
         queue.clear();
     }
-    public void getNextBall() {
-        //TODO add this function
+    private boolean ballCheck = false;
+    public void ballCheck() {
+
+
+    }
+    enum LaunchSequenceSteps {
+        READY, LAUNCH
+    }
+    private boolean launchStart = false;
+    private boolean doneLaunch = false;
+    private LaunchSequenceSteps launchSequenceSteps = LaunchSequenceSteps.READY;
+    public void launchSequence() {
+        switch (launchSequenceSteps) {
+            case READY:
+                ballCheck();
+                if (launchStart) {
+                    if (ballCheck) {
+                        launchSequenceSteps = LaunchSequenceSteps.LAUNCH;
+                    }
+                }
+                break;
+
+            case LAUNCH:
+
+                if (doneLaunch == true) {
+                    launchSequenceSteps = LaunchSequenceSteps.READY;
+                }
+                break;
+        }
     }
     public void update() {
         //TODO add this function
